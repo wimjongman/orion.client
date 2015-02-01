1d88a3ddb02d62a1b45bdb22c9bd55867ac2f9ee
Brandon Dalesandro <brandonadalesandro@gmail.com>
2015-02-01 14:29:09 -0800

    Separated the tests for the nodegit api, and our git api
    Signed-off-by: Brandon Dalesandro <brandonadalesandro@gmail.com>

--- modules/orionode/test/test-git-api.js
+++ modules/orionode/test/test-git-api.js
@@ -12,23 +12,56 @@
 var assert = require("assert");
 var mocha = require("mocha");
 var request = require("supertest");
-var clone = require("nodegit").Clone.clone
 
-var fs = require("fs");
+var path = require("path");
+var testData = require("./support/test_data");
 
-var clone_dir = "test/support/repo/";
+var WORKSPACE = path.join(__dirname, ".test_workspace");
 
+var orion = require("../");
 
-describe("nodegit", function() {
-		// Clone a given repository into a tmp folder.
-	describe("clone", function(){
-		it("can clone with http", function() {
-		    var url = "http://github.com/nodegit/test.git";
-		    var opts = { ignoreCertErrors: 1 };
-		    assert.equal(true, true);
-		    /*return clone(url, clone_dir, opts).then(function(repository) {
-		      assert.ok(repository instanceof Repository);
-		    });*/
+describe("Git API", function() {
+	var app;
+	beforeEach(function(done) {
+		app = testData.createApp();
+		testData.setUp(WORKSPACE, done);
+	});
+
+	describe("options", function() {
+		it("demands workspaceDir", function(done) {
+			try {
+				assert.throws(function() {
+					orion();
+				});
+			} catch (e) {
+				done(e);
+			}
+			done();
+		});
+		it("accepts cache-max-age", function(done) {
+			app.use(orion({
+				workspaceDir: WORKSPACE,
+				maxAge: 31337 * 1000 // ms
+			}))
+			.request()
+			.get("/index.html")
+			.expect("cache-control", /max-age=31337/, done); //seconds
+		});
+		// TODO test configParams once they are cleaned up/merged with options
+	});
+
+	describe("api", function() {
+		//	Writing tests for orion
+		//	for each thing the api should do add a
+		//	=>it("description", callback)
+		//	you should follow the pattern shown below closely, just changing the .get request, and the .expect
+		it("should get the workspace", function(done) {
+			app.use(orion({
+				workspaceDir: WORKSPACE
+			}))
+			.request()
+			.get("/gitapi/clone/workspace/")
+			.expect(200, done);
 		});
 	});
 });
