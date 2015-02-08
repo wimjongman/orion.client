7f64a4c551362155c1cc988964e81d27bc217107
Mei-Vern Then <mt2837@columbia.edu>
2015-02-07 19:25:55 -0500

    Added more code to status.js and made changes to git.js.
    Signed-off-by: Mei-Vern Then <mt2837@columbia.edu>

--- modules/orionode/lib/git/status.js
+++ modules/orionode/lib/git/status.js
@@ -1,53 +1,94 @@
 /*******************************************************************************
  * Copyright (c) 2012 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials are made 
- * available under the terms of the Eclipse Public License v1.0 
- * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
- * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html).
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 /*eslint-env node */
-var api = require('../api'), writeError = api.writeError;
+var api = require('../api'),
+    writeError = api.writeError;
 var git = require('nodegit');
 var finder = require('findit');
 var path = require("path");
 
-function getStatus(workspaceDir, fileRoot, req, res, next, rest) {				
-		var status = [];
-		var repoPath = rest.replace("status/file/", "");
-		repoPath = api.join(workspaceDir, repoPath);
-		console.log(repoPath);
-		git.Repository.open(repoPath)
-		  .then(function(repo) {
-		    repo.getStatus().then(function(statuses) {
-		      function statusToText(status) {
-		        var added = [],
-		        	changed = [],
-		        	conflicting = [],
-		        	missing = [],
-		        	modified = [],
-		        	removed = [],
-		        	untracked = [];
-		        	
-		        if (status.isNew()) { untracked.push("NEW"); }
-		        if (status.isModified()) { modified.push("MODIFIED"); }
-		        if (status.isTypechange()) { words.push("TYPECHANGE"); }
-		        if (status.isRenamed()) { words.push("RENAMED"); }
-		        if (status.isIgnored()) { words.push("IGNORED"); }
-		
-		        return words.join(" ");
-		      }
-		
-		      statuses.forEach(function(file) {
-		        console.log(file.path() + " " + statusToText(file));
-		      });
-		    });
-		});
+function getStatus(workspaceDir, fileRoot, req, res, next, rest) {
+    var status = [];
+    var repoPath = rest.replace("status/file/", "");
+    repoPath = api.join(workspaceDir, repoPath);
+    git.Repository.open(repoPath)
+        .then(function(repo) {
+            if (repo) {
+                repo.getStatus().then(function(statuses) {
+                    var added = [],
+                        changed = [], // no idea
+                        conflicting = [], // merge conflict??
+                        missing = [], // no idea
+                        modified = [],
+                        removed = [], // no idea
+                        untracked = [];
+
+                    function returnContent(file) {
+                    	var orionFilePath = api.join(rest.replace("status/file/", ""), file.path().replace(workspaceDir,""));
+                    	console.log("workspaceDir=" + workspaceDir);
+                    	console.log("originalFilePath=" + file.path());
+                    	console.log("orionFilePath="+orionFilePath);
+                        return {
+                            "Git": {
+                                "CommitLocation": "/gitapi/commit/HEAD/file/" + orionFilePath,
+                                "DiffLocation": "/gitapi/diff/Default/file/"+ orionFilePath,
+                                "IndexLocation": "/gitapi/index/file/" + orionFilePath
+                            },
+                            "Location": "/file/" + orionFilePath,
+                            "Name": file.path(),
+                            "Path": file.path()
+                        }
+                    }
+                    statuses.forEach(function(file) {
+                        if (file.isNew()) {
+                            untracked.push(returnContent(file));
+                        }
+                        if (file.isModified()) {
+                            modified.push(returnContent(file));
+                        }
+
+                        //		        if (status.isTypechange()) { words.push("TYPECHANGE"); }
+                        //		        if (status.isRenamed()) { words.push("RENAMED"); }
+                        //		        if (status.isIgnored()) { words.push("IGNORED"); }
+                        //		        console.log(file.path() + " " + statusToText(file));
+                    });
+                    var resp = JSON.stringify({
+                        "Added": added,
+                        "Changed": changed,
+                        "CloneLocation": "/gitapi/clone/file/" + rest.replace("status/file/", ""),
+                        "CommitLocation": "/gitapi/commit/HEAD/file/" + rest.replace("status/file/", ""),
+                        "Conflicting": conflicting,
+                        "IndexLocation": "/gitapi/index/file/" + rest.replace("status/file/", ""),
+                        "Location": "/gitapi/status/file/" + rest.replace("status/file/", ""),
+                        "Missing": missing,
+                        "Modified": modified,
+                        "Removed": removed,
+                        "RepositoryState": "SAFE",
+                        "Type": "Status",
+                        "Untracked": untracked
+                        
+                    });
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.setHeader('Content-Length', resp.length);
+                    res.end(resp);
+
+                });
+            } else {
+                writeError(403, res);
+            }
+        });
 
 }
 
 module.exports = {
-	getStatus: getStatus
+    getStatus: getStatus
 };
