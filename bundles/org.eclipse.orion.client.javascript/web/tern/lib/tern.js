39b5672ffa3a73bad6a1205192e336e8fb780ead
Mike Rennie <Michael_Rennie@ca.ibm.com>
2015-03-04 19:47:05 -0600

    Add acorn.walk + export AMD
    Change-Id: I7622906e35165e773284a878b6dd456047cc97a9

--- bundles/org.eclipse.orion.client.javascript/web/tern/lib/tern.js
+++ bundles/org.eclipse.orion.client.javascript/web/tern/lib/tern.js
@@ -4,14 +4,14 @@
 // project, and defines an interface for querying the code in the
 // project.
 
-/* eslint-disable */
-define([
-"exports", 
-"./infer", 
-"./signal", 
-"esprima", 
-"acorn/util/walk"
-], function(exports, infer, signal, acorn, walk) {
+(function(mod) {
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    return mod(exports, require("./infer"), require("./signal"),
+               require("acorn/acorn"), require("acorn/util/walk"));
+  if (typeof define == "function" && define.amd) // AMD
+    return define(["exports", "./infer", "./signal", "acorn/acorn", "acorn/util/walk"], mod);
+  mod(self.tern || (self.tern = {}), tern, tern.signal, acorn, acorn.walk); // Plain browser env
+})(function(exports, infer, signal, acorn, walk) {
   "use strict";
 
   var plugins = Object.create(null);
@@ -890,5 +890,4 @@ define([
   }
 
   exports.version = "0.6.2";
-  return exports;
 });
