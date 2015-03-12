b744ac03c4a40f286c86f0f2488b8e8c720c04c7
Mike Rennie <Michael_Rennie@ca.ibm.com>
2015-03-12 16:22:16 -0400

    content assist message passing + sync + file client callbacks

--- bundles/org.eclipse.orion.client.javascript/web/javascript/contentAssist/tern/ternContentAssist.js
+++ bundles/org.eclipse.orion.client.javascript/web/javascript/contentAssist/tern/ternContentAssist.js
@@ -13,14 +13,7 @@
 define([
 	'orion/Deferred',  //$NON-NLS-0$
 	'orion/objects',  //$NON-NLS-0$
-	'tern/lib/tern',  //$NON-NLS-0$
-	'tern/plugin/doc_comment', //TODO must load them, they self-register with Tern
-	'tern/plugin/requirejs',
-	'orion/fileClient',
-	'javascript/hover',
-	'tern/defs/ecma5',
-	'tern/defs/browser'
-], /* @callback */ function(Deferred, Objects, Tern, docPlugin, requirePlugin, FileClient, Hover, ecma5, browser) {
+], /* @callback */ function(Deferred, Objects) {
 
 	/**
 	 * @description Creates a new TernContentAssist object
@@ -28,12 +21,13 @@ define([
 	 * @public
 	 * @param {orion.FileClient} fileClient The Orion fileClient 
 	 * @param {javascript.ASTManager} astManager An AST manager to create ASTs with
+	 * @param {Object} ternWorker The Tern worker
 	 * @since 9.0
 	 */
 	function TernContentAssist(fileClient, astManager, ternWorker) {
 	    this.fileclient = fileClient;
-	    var _self = this;
 		this.astManager = astManager;
+		this.ternworker = ternWorker;
 	}
 
 	/**
@@ -41,171 +35,28 @@ define([
 	 */
 	Objects.mixin(TernContentAssist.prototype, {
 
-        lastFile: null,
-        server: null,
-        
-		/**
-		 * Called by the framework to initialize this provider before any <tt>computeContentAssist</tt> calls.
-		 */
-		initialize: function() {
-		    this._getServer();
-		},
-        
 		/**
 		 * @description Implements the Orion content assist API v4.0
 		 */
 		computeContentAssist: function(editorContext, params) {
 		    var that = this;
 		    return editorContext.getFileMetadata().then(function(meta) {
-		        return that._getServer().then(function(server) {
-		           var d = new Deferred();
-		           var proposals = [];
-    			   if(server) {
-    			       server.request({query: {
-    			                         type: "completions", 
-    			                         file: meta.location,
-    			                         types: true, 
-    			                         origins: true,
-    			                         urls: true,
-    			                         docs: true,
-    			                         end: params.offset,
-    			                         guess: true
-    			                       }}, 
-    			                         function(error, comps) {
-    			                             if(error) {
-    			                                 d.reject(error);
-    			                             }
-    			                             if(comps && comps.completions) {
-    			                                 var completions = comps.completions;
-    			                                 for(var i = 0; i < completions.length; i++) {
-    			                                     var completion = completions[i];
-        			                                 proposals.push(that._formatTernProposal(completion));
-            							         }
-            							         d.resolve(proposals);
-    			                             }
-    			                         }
-    			                      );
-    			   } 
-    			   return d;
-    			});
+	           var d = new Deferred();
+	           if(that.ternworker) {
+	               that.ternworker.addEventListener('message', function(event) {
+	                   var _d = event.data;
+	                   if(typeof(_d) === 'object') {
+	                       if(_d.request === 'assist') {
+	                           d.resolve(_d.proposals);
+	                       }
+	                   } else if(!_d) {
+	                       d.reject('No proposals');
+	                   }
+	               });
+	               that.ternworker.postMessage({args: {meta: meta, params: params}, request: 'assist'});
+	               return d;
+	           }
 		    });
-		},
-		
-		/**
-		 * @name _formatTernProposal
-		 * @description Formats the proposal
-		 * @function
-		 * @private
-		 * @param {tern.Completion} completion The Tern proposal object
-		 * @returns {orion.Proposal} An Orion-formatted proposal object
-		 */
-		_formatTernProposal: function _formatTernProposal(completion) {
-		    var proposal = {
-                relevance: 100,
-                style: 'emphasis',
-                overwrite: true
-            };
-            proposal.name = completion.name;
-            if(typeof(completion.type) !== 'undefined') {
-                var type = completion.type.replaceAll('->', ':');
-                type = type.replaceAll('?', 'Any');
-                if(/^fn/.test(type)) {
-                    proposal.name = completion.name + type.slice(2);
-                } else {
-                    switch(completion.type) {
-        		        case '{}': {
-        		            proposal.name += ' : Object';
-        		            break;
-        		        }
-        		        case 'number': {
-        		            proposal.name += ' : Number';
-        		            break;
-        		        }
-        		        case 'string': {
-        		            proposal.name += ' : String';
-        		            break;
-        		        }
-        		        case '?': {
-        		            proposal.name += ' : Any';
-        		            break;
-        		        } 
-        		        default: null;
-        		    }
-    		    }
-            }
-            proposal.proposal = proposal.name;
-            var obj = Object.create(null);
-            obj.type = 'markdown';
-            obj.content = '';
-            if(!completion.doc) {
-                obj.content += proposal.name;
-            } else {
-                obj.content += Hover.formatMarkdownHover(completion.doc).content;
-            }
-            if(completion.url) {
-                obj.content += '\n\n[Online documentation]('+completion.url+')';
-            }
-            proposal.hover = obj;
-            return proposal;
-		},
-		
-		/**
-		 * @name _startServer
-		 * @description Starts the Tern server in a Deferred
-		 * @function
-		 * @private
-		 */
-		_getServer: function _getServer() {
-		    return new Deferred().resolve(this._startServer());
-		},
-		
-		/**
-		 * @name _startServer
-		 * @description Starts the Tern server if not done so
-		 * @function
-		 * @private
-		 * @returns {tern.Server} The new or existing Tern server
-		 */
-		_startServer: function _startServer() {
-		    if(!this.server) {
-		        var options = {
-    		        async: true,
-    		        debug:true,
-    		        defs: [ecma5, browser],
-    		        projectDir: '/',
-    		        plugins: {
-    		            doc_comment: {
-    		                fullDocs: true
-    		            },
-    		            requirejs: {
-    		                baseURL: '/'
-    		            }
-    		        },
-    		        getFile: this._getFile.bind(this)
-    		    };
-    		    this.server = new Tern.Server(options);
-		    }
-	        return this.server;
-		},
-		
-		/**
-		 * @name _getFile
-		 * @description Tern server callback
-		 * @function
-		 * @private
-		 * @param {String} file The file to load
-		 * @param {Function} callback The loaded callback
-		 * @returns {Object} The file object
-		 */
-		_getFile: function _getFile(file, callback) {
-		    if(this.server) {
-	           return this.fileclient.read(file).then(function(f) {
-	               callback(null, f);
-	           },
-	           function(error) {
-	               callback(error, null);
-	           });
-		    }
 		},
 		
 		/**
@@ -213,11 +64,9 @@ define([
 		 * @param {Object} event
 		 */
 		updated: function(event) {
-		    this._getServer().then(function(server) {
-		        if(event.file.location) {
-    		        server.delFile(event.file.location);
-    		    }
-		    });
+	        if(this.ternworker && event.file.location) {
+		        this.ternworker.postMessage({request: 'delfile', file: event.file.location});
+		    }
 		}
 		
 	});
