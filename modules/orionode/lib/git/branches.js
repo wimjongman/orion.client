757623b7f888b9d4f74d6f06ba53a0874c9b409a
Albert Cui <aqc2109@columbia.edu>
2015-05-02 02:44:59 -0400

    Fix switching between remotes.
    Signed-off-by: Albert Cui <aqc2109@columbia.edu>

--- modules/orionode/lib/git/branches.js
+++ modules/orionode/lib/git/branches.js
@@ -12,83 +12,96 @@
 var api = require('../api'), writeError = api.writeError;
 var git = require('nodegit');
 var fs = require('fs');
+var async = require('async');
 
 function getBranches(workspaceDir, fileRoot, req, res, next, rest) {
 	var repoPath = rest.replace("branch/file/", "");
 	var fileDir = repoPath;
 	var gitPath;
     repoPath = api.join(workspaceDir, repoPath);
+
+    var theRepo;
+	var remotes = [];
+	var branches = [];
+
 	git.Repository.open(repoPath)
 	.then(function(repo) {
-		repo.getReferences(git.Reference.TYPE.OID)
-		.then(function(referenceList) {
-			var remotes = []
-			var branches = {}
-	 		referenceList.forEach(function(ref) {
- 				if (ref.isRemote()) {
-					remoteName = ref.name().replace("refs/remotes/", "");
-					remotes.push(remoteName);
-				}
+		theRepo = repo;
+		return repo.getReferences(null);
+	})		
+	.then(function(referenceList) {
+ 		referenceList.forEach(function(ref) {
+ 			if (ref.isBranch()) {
+ 				var branchURL = ref.name().split("/").join("%252F");
+ 				var branchName = ref.name().replace("refs/heads/", "");
+ 				var isCurrent = ref.isHead() ? true : false;
 
-	 			if (ref.isBranch()) {
-	 				var branchURL = ref.name().split("/").join("%252F");
-	 				var branchName = ref.name().replace("refs/heads/", "");
-	 				var isCurrent = ref.isHead() ? true : false;
+ 				branches.push({
+		 			"CloneLocation": "/gitapi/clone/file/"+ fileDir,
+		 			"CommitLocation": "/gitapi/commit/" + branchURL + "/file/" + fileDir,
+		 			"Current": isCurrent,
+		 			"DiffLocation": "/gitapi/diff/" + branchName + "/file/" + fileDir,
+		 			"FullName": "refs/heads/" + branchName,
+		 			"HeadLocation": "/gitapi/commit/HEAD/file/" + fileDir,
+		 			"LocalTimeStamp": 1424471958000, //hardcoded local timestamp
+		 			"Location": "/gitapi/branch/" + branchName + "/file/" + fileDir,
+		 			"Name": branchName,
+		 			"RemoteLocation": [],
+		 			"TreeLocation": "/gitapi/tree/file/" + fileDir + "/" + branchName,
+		 			"Type": "Branch"
+	 			});
+ 			}
+		});
 
-	 				branches[branchName] = {
-			 			"CloneLocation": "/gitapi/clone/file/"+ fileDir,
-			 			"CommitLocation": "/gitapi/commit/" + branchURL + "/file/" + fileDir,
-			 			"Current": isCurrent,
-			 			"DiffLocation": "/gitapi/diff/" + branchName + "/file/" + fileDir,
-			 			"FullName": "refs/heads/" + branchName,
-			 			"HeadLocation": "/gitapi/commit/HEAD/file/" + fileDir,
-			 			"LocalTimeStamp": 1424471958000, //hardcoded local timestamp
-			 			"Location": "/gitapi/branch/" + branchName + "/file/" + fileDir,
-			 			"Name": branchName,
-			 			"RemoteLocation": [],
-			 			"TreeLocation": "/gitapi/tree/file/" + fileDir + "/" + branchName,
-			 			"Type": "Branch"
-		 			};
-	 			}
- 			});
-			//Check if there are remotes and branches of the same name
-			remotes.forEach(function(remote) {
-				var nameToMatch = remote.substring(remote.indexOf("/") + 1);
-				var remoteURL = remote.split("/").join("%252F");
-				var remoteName = remote.substring(0, remote.indexOf("/"));
-				if (branches[nameToMatch]) {
-					branches[nameToMatch]["RemoteLocation"].push({
+		return git.Remote.list(theRepo);
+ 	})
+ 	.then(function(remotes) {
+		// It appears that the java server just generates a remote object for each remote that exists,
+		// regardless if there is actually a remote tracking branch that exists.
+		async.each(remotes, function(remote, cb) {
+			git.Remote.lookup(theRepo, remote)
+			.then(function(remote) {
+				var remoteName = remote.name();
+
+				branches.forEach(function(branch) {
+					var remoteBranchName = api.join(remoteName, branch["Name"]);
+
+					branch["RemoteLocation"].push({
 				        "Children": [{
-				          "CloneLocation": branches[nameToMatch]["CloneLocation"],
-				          "CommitLocation": "/gitapi/commit/" + remoteURL + "/file/" + fileDir,
-				          "DiffLocation": "/gitapi/diff/" + remoteURL + "/file/" + fileDir,
-				          "FullName": "refs/remotes/" + remote,
-				          "GitUrl": "git@github.com:albertcui/orion.client.git", //hardcoded
-				          "HeadLocation": "/gitapi/commit/HEAD/" + fileDir,
-				          "Id": "08c41c776f2e9e5dca7fbbbcfe2c8c9c06300998", //hardcoded
-				          "IndexLocation": "/gitapi/index/" + fileDir,
-				          "Location": "/gitapi/remote/" + remote + "/file/" + fileDir,
-				          "Name": "origin/node_git_pages",
-				          "TreeLocation": "/gitapi/tree/file/" + fileDir + "/" + remoteURL,
+				          // "CloneLocation": branch["CloneLocation"],
+				          // "CommitLocation": "/gitapi/commit/" + remoteBranchName + "/file/" + fileDir,
+				          // "DiffLocation": "/gitapi/diff/" + remoteBranchName + "/file/" + fileDir,
+				          "FullName": "refs/remotes/" + remoteName,
+				          "GitUrl": remote.url(),
+				          // "HeadLocation": "/gitapi/commit/HEAD/" + fileDir,
+				          //"Id": id, is listed in wiki, but not in Java server
+				          // "IndexLocation": "/gitapi/index/" + fileDir,
+				          "Location": "/gitapi/remote/" + remoteBranchName + "/file/" + fileDir,
+				          "Name": remoteBranchName,
+				          // "TreeLocation": "/gitapi/tree/file/" + fileDir + "/" + remoteBranchName,
 				          "Type": "RemoteTrackingBranch"
 				        }],
-				        "CloneLocation": "/gitapi/clone/file/" + fileDir,
-				        "GitUrl": "git@github.com:albertcui/orion.client.git",
+				        "CloneLocation": branch["CloneLocation"],
+				        "GitUrl": remote.url(),
 				        "IsGerrit": false, //hardcoded
-				        "Location": "/gitapi/remote/" + remoteName + "/" + fileDir,
-				        "Name": "origin",
+				        "Location": "/gitapi/remote/" + remoteName + "/file/" + fileDir,
+				        "Name": remoteName,
 				        "Type": "Remote"
 					})
-				}
-			});
-			var branchesArray = [];
+				})
 
-			for(var prop in branches) {
-				branchesArray.push(branches[prop]);
+				cb();
+			})
+			.catch(function(err) {
+				cb(err);
+			})
+		}, function(err) {
+			if (err) {
+				console.log(err)
+				return writeError(500, res);
 			}
-
 			var resp = JSON.stringify({
-				"Children": branchesArray,
+				"Children": branches,
 				"Type": "Branch"
 			});
 		
@@ -96,8 +109,12 @@ function getBranches(workspaceDir, fileRoot, req, res, next, rest) {
 			res.setHeader('Content-Type', 'application/json');
 			res.setHeader('Content-Length', resp.length);
 			res.end(resp);
- 		})
- 	})
+		});
+	})
+	.catch(function(err) {
+		console.log(err);
+		writeError(500, res);
+	})
 }
 
 function getBranchMetadata(workspaceDir, fileRoot, req, res, next, rest) {
