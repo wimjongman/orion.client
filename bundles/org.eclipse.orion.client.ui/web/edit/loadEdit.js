32717200b41f98516edf59fb5b1e4c6084af584d
Bogdan Gheorghe <gheorghe@ca.ibm.com>
2015-06-10 12:30:56 -0400

    Remove inline JS from edit.html

---
+++ bundles/org.eclipse.orion.client.ui/web/edit/loadEdit.js
@@ -0,0 +1,10 @@
+/*eslint-env browser, amd*/
+require({
+	baseUrl: '..',
+	paths: {
+		text: 'requirejs/text',
+		i18n: 'requirejs/i18n',
+		domReady: 'requirejs/domReady'    
+	}
+});
+require(["edit/edit"]);
